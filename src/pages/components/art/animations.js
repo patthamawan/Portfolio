e9d441302bd7b47cabc5cf612775a740129dd141
patthamawan@zanroo.com <patthamawan@zanroo.com>
2020-09-14 09:11:35 +0100

    videos

--- src/pages/components/art/animations.js
+++ src/pages/components/art/animations.js
@@ -9,10 +9,24 @@ const animation = (props) => {
       <React.Fragment>
         <div className="container">
           <div className="intro">
-            <h1>Videos are coming Soon!</h1>
+            <h1>Animations</h1>
+            <p>Videos sample of work.</p>
           </div>
-          <div className="back-btn">
-            <a href="/">BACK</a>
+          <div className="video-list">
+            <Row>
+              <Col md={6} sm={12}>
+                <iframe src="https://player.vimeo.com/video/371390502" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+              </Col>
+              <Col md={6} sm={12}>
+                <iframe src="https://player.vimeo.com/video/455828452"  frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+              </Col>
+              <Col md={6} sm={12}>
+                <iframe src="https://player.vimeo.com/video/455616431"  frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+              </Col>
+              <Col md={6} sm={12}>
+                <iframe src="https://player.vimeo.com/video/427054381"  frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+              </Col>
+            </Row>
           </div>
         </div>
         </React.Fragment>
